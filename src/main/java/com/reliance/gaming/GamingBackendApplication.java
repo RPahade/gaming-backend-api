b0b6bb252ae332ff940b4868b0b6f10972aac4fb
RPahade <rohpahade2@gmail.com>
2026-01-04 14:07:53 +0530

    Initial project setup with Spring Boot and dependencies

---
+++ src/main/java/com/reliance/gaming/GamingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.reliance.gaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingBackendApplication.class, args);
+	}
+
+}
